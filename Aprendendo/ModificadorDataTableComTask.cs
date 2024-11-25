2246b54e1c06706948e4c1fcb07f795825b29494
Paulo Neto <31972103+netiim@users.noreply.github.com>
2024-11-25 07:08:46 -0300

    Ajustes para quando vier os próxcimos teste

--- Aprendendo/ModificadorDataTableComTask.cs
+++ Aprendendo/ModificadorDataTableComTask.cs
@@ -1,17 +1,41 @@
 ﻿using CsvHelper.Configuration;
 using CsvHelper;
-using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace Aprendendo
 {
     public static class ModificadorDataTableComTask
     {
+        public static void ValidarHipotese()
+        {
+            // Inicia o cronômetro para medir o tempo total
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            // Inicia o cronômetro para medir o tempo de carregamento do DataTable
+            Stopwatch loadTime = Stopwatch.StartNew();
+            DataTable table = ModificadorDataTableComTask.LoadCsvToDataTable("./rais-2021.csv");
+            loadTime.Stop();
+            Console.WriteLine($"Tempo para carregar o DataTable: {loadTime.ElapsedMilliseconds}ms");
+
+            // Inicia o cronômetro para medir o tempo de modificação da coluna com Task
+            Stopwatch modifyTimeParalelo = Stopwatch.StartNew();
+            ModificadorDataTableComTask.ModificarColunaDataTableComParalelismo(table, "Nome da região");
+            modifyTimeParalelo.Stop();
+            Console.WriteLine($"Tempo para modificar com Paralelismo a coluna 'Nome da região': {modifyTimeParalelo.ElapsedMilliseconds}ms");
+
+            //Inicia o cronômetro para medir o tempo de modificação da coluna da forma tradicional
+            Stopwatch modifyTime = Stopwatch.StartNew();
+            ModificadorDataTableComTask.ModificarColunaDataTable(table, "Nome da região");
+            modifyTime.Stop();
+            Console.WriteLine($"Tempo para modificar a coluna 'Nome da região': {modifyTime.ElapsedMilliseconds}ms");
+
+            // Finaliza o cronômetro total
+            stopwatch.Stop();
+            Console.WriteLine($"Tempo total: {stopwatch.ElapsedMilliseconds}ms");
+        }
+
         public static DataTable LoadCsvToDataTable(string filePath)
         {
             var dataTable = new DataTable();
--- Aprendendo/Program.cs
+++ Aprendendo/Program.cs
@@ -1,31 +1,3 @@
-﻿using System.Data;
-using System.Globalization;
-using CsvHelper;
-using CsvHelper.Configuration;
-using System.Diagnostics;
-using Aprendendo;
+﻿using Aprendendo;
 
-// Inicia o cronômetro para medir o tempo total
-Stopwatch stopwatch = Stopwatch.StartNew();
-
-// Inicia o cronômetro para medir o tempo de carregamento do DataTable
-Stopwatch loadTime = Stopwatch.StartNew();
-DataTable table = ModificadorDataTableComTask.LoadCsvToDataTable("./rais-2021.csv");
-loadTime.Stop();
-Console.WriteLine($"Tempo para carregar o DataTable: {loadTime.ElapsedMilliseconds}ms");
-
-// Inicia o cronômetro para medir o tempo de modificação da coluna com Task
-Stopwatch modifyTimeParalelo = Stopwatch.StartNew();
-ModificadorDataTableComTask.ModificarColunaDataTableComParalelismo(table, "Nome da região");
-modifyTimeParalelo.Stop();
-Console.WriteLine($"Tempo para modificar com Paralelismo a coluna 'Nome da região': {modifyTimeParalelo.ElapsedMilliseconds}ms");
-
-//Inicia o cronômetro para medir o tempo de modificação da coluna da forma tradicional
-Stopwatch modifyTime = Stopwatch.StartNew();
-ModificadorDataTableComTask.ModificarColunaDataTable(table, "Nome da região");
-modifyTime.Stop();
-Console.WriteLine($"Tempo para modificar a coluna 'Nome da região': {modifyTime.ElapsedMilliseconds}ms");
-
-// Finaliza o cronômetro total
-stopwatch.Stop();
-Console.WriteLine($"Tempo total: {stopwatch.ElapsedMilliseconds}ms");
+ModificadorDataTableComTask.ValidarHipotese();
